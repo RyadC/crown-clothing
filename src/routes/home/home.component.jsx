2e8c2c8d34774bb9d7266414414c7a2b82dca4a6
RyadC <r.chair@hotmail.fr>
2024-08-24 22:09:59 +0200

    :sparkles: feat(home): add Home component && routes folder

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,40 @@
+import Directory from "../../components/directory/directory.component";
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl:
+        "https://images.pexels.com/photos/1878821/pexels-photo-1878821.jpeg",
+    },
+    {
+      id: 1,
+      title: "Jackets",
+      imageUrl:
+        "https://images.pexels.com/photos/26585488/pexels-photo-26585488.jpeg",
+    },
+    {
+      id: 1,
+      title: "Snickers",
+      imageUrl:
+        "https://images.pexels.com/photos/233312/pexels-photo-233312.jpeg",
+    },
+    {
+      id: 1,
+      title: "Womens",
+      imageUrl:
+        "https://images.pexels.com/photos/1755674/pexels-photo-1755674.jpeg",
+    },
+    {
+      id: 1,
+      title: "Mens",
+      imageUrl:
+        "https://images.pexels.com/photos/1549004/pexels-photo-1549004.jpeg",
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
+
+export default Home;
