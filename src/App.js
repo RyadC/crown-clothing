3a641a1105221fa727fa77888021dbf8003d3293
RyadC <r.chair@hotmail.fr>
2024-11-22 22:21:42 +0100

    :sparkles: feat(app & index): add a redux provider & add the user connexion listener in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { onAuthStateChangedListener } from "./utils/firebase/firebase.utils";
+
+import { setCurrentUserAction } from "./store/user/user.action";
+
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
@@ -6,6 +13,24 @@ import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // Open the Listener after the component was mounted
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      console.log(user);
+
+      if (user) {
+        dispatch(setCurrentUserAction(user));
+      } else {
+        dispatch(setCurrentUserAction(null));
+      }
+    });
+
+    // Destroy the listener after the component was unmounted
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
