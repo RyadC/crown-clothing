338afd2bec0585959bfd59ec876475d428d46438
RyadC <r.chair@hotmail.fr>
2024-12-01 22:33:19 +0100

    :sparkles: feat(store): add persisting data in localStorage with Redux-Persist

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 
 import App from "./App";
 
@@ -15,7 +16,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
