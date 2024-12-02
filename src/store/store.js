1f1518e13de223bd86afadb3cfef80fa372a4d75
RyadC <r.chair@hotmail.fr>
2024-12-02 15:32:23 +0100

    :alembic: test(store): add redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -13,9 +13,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [logger];
+const middlewares = [process.env.NODE_ENV !== "production " && logger].filter(
+  Boolean
+); // We log only in a development environment. filter inject the value if is a truthy value in a shallow copy. Boolean is a class that takes "true" or "false" as argument. So without bracket, he take the current value of the array.
 
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const composedEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose; // We take the composer of the Redux DevTools Extension otherwise we take the compose of redux. So we can use the Redux Devtools
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middlewares));
 
 export const store = createStore(
   persistedReducer,
